1fddad2598f2fd6464e097a6c2fda0a7cc9db10f
Hwanju Lee <hwanju1596@gmail.com>
2022-11-25 16:51:46 +0900

    #register-ui 1. 버튼 눌렀을 때 인증번호 입력하는 text field 나오게 함.

--- pages/auth/certification-phone.tsx
+++ pages/auth/certification-phone.tsx
@@ -1,8 +1,58 @@
+import { Button, FormControl, FormHelperText, Input, InputAdornment } from "@mui/material";
+import { Stack } from "@mui/system";
+import {
+  MuiTelInput,
+  MuiTelInputContinent,
+  MuiTelInputCountry,
+  MuiTelInputInfo,
+} from "mui-tel-input";
 import React from 'react'
 
 export const CertificaationPhone = () => {
-    
+  const [phoneNumber, setPhoneNumber] = React.useState<string>("");
+  const [viewAuthSection, setViewAuthSection] = React.useState<boolean>(false);
+  const handlePhoneNumberChange = (newPhone: string, info: MuiTelInputInfo) => {
+    setPhoneNumber(newPhone);
+  };
+  // const continents: MuiTelInputContinent[] = ["AS"];
+  const excludedCountries: MuiTelInputCountry[] = [];
+
   return (
-    <div>CertificaationPhone</div>
+    <Stack spacing={2}>
+      <MuiTelInput
+        fullWidth
+        defaultCountry="KR"
+        value={phoneNumber}
+        onChange={handlePhoneNumberChange}
+        // continents={continents}
+        excludedCountries={excludedCountries}
+      />
+      {viewAuthSection &&
+        <FormControl variant="standard">
+          <Input
+            id="auth-input"
+          />
+          <FormHelperText id="auth-count-down">3:00</FormHelperText>
+        </FormControl>
+        }
+      <Button
+        color="primary"
+        fullWidth
+        type="submit"
+        variant="contained"
+        onClick={() => setViewAuthSection(true)}
+      >
+        인증하기
+      </Button>
+    </Stack>
+
   )
+}
+
+const CountDownButton = () => {
+
+}
+
+const NumberInputCompleteButton = () => {
+
 }
