05bc3297082935aa8ae675a954e1f191dd60d86f
Hwanju Lee <hwanju1596@gmail.com>
2022-11-26 00:47:55 +0900

    #register-ui 1. 인증유효시간 timer 적용

--- pages/auth/certification-phone.tsx
+++ pages/auth/certification-phone.tsx
@@ -1,4 +1,10 @@
-import { Button, FormControl, FormHelperText, Input, InputAdornment } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputAdornment,
+} from "@mui/material";
 import { Stack } from "@mui/system";
 import {
   MuiTelInput,
@@ -6,7 +12,7 @@ import {
   MuiTelInputCountry,
   MuiTelInputInfo,
 } from "mui-tel-input";
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export const CertificaationPhone = () => {
   const [phoneNumber, setPhoneNumber] = React.useState<string>("");
@@ -27,32 +33,58 @@ export const CertificaationPhone = () => {
         // continents={continents}
         excludedCountries={excludedCountries}
       />
-      {viewAuthSection &&
+      {viewAuthSection && (
         <FormControl variant="standard">
-          <Input
-            id="auth-input"
-          />
+          <Input id="auth-input" />
           <FormHelperText id="auth-count-down">3:00</FormHelperText>
         </FormControl>
-        }
-      <Button
-        color="primary"
-        fullWidth
-        type="submit"
-        variant="contained"
-        onClick={() => setViewAuthSection(true)}
-      >
-        인증하기
-      </Button>
-    </Stack>
+      )}
 
-  )
-}
+      <InitButton />
+    </Stack>
+  );
+};
+
+const AuthSection = () => {
+  return <></>;
+};
+
+const InitButton = () => {
+  return (
+    <Button
+      color="primary"
+      fullWidth
+      type="submit"
+      variant="contained"
+      onClick={() => setViewAuthSection(true)}
+    >
+      인증하기
+    </Button>
+  );
+};
 
 const CountDownButton = () => {
+  const [time, setTime] = useState<number>(-1);
+  var secTime = 180;
+  useEffect(() => {
+    const countDown = setInterval(() => {
+      setTime(secTime);
+      console.log(secTime);
+      if (secTime < 0) {
+        clearInterval(countDown);
+      }
+      --secTime;
+    }, 1000);
+    return () => clearInterval(countDown);
+  }, time);
 
-}
+  return (
+    <Button fullWidth type="submit" variant="contained" color="success">
+      {time}
+    </Button>
+  );
+};
 
 const NumberInputCompleteButton = () => {
-
-}
+  return <></>;
+};
