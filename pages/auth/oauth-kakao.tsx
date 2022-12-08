386033ea101765974c24cd6b310ca97c91cbf77e
Hwanju <hwanju1596@gmail.com>
2022-12-08 11:24:48 +0900

    #outh-kakao 1. 간편로그인 구현.

--- pages/auth/oauth-kakao.tsx
+++ pages/auth/oauth-kakao.tsx
@@ -4,9 +4,12 @@ import Image from 'next/image';
 import KakaoLoginButton from "../../public/kakao_login_medium_wide.png";
 
 const OAuthKakao = () => {
+    const REST_API_KEY = "02ddaa97e21deb004a41e0f09dc46db1";
+    const REDIRECT_URI = "http://localhost:3000/";
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
     return (<>
-        <Button>
+        <Button href={KAKAO_AUTH_URL}>
             <Image
                 src={KakaoLoginButton}
                 alt="kakao login"
