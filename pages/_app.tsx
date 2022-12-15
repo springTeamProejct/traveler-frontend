3fe9f2e41002d9a3732988eb1b1c111b58546076
Hwanju <hwanju1596@gmail.com>
2022-12-15 13:42:08 +0900

    이전 구조 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { VMOauthKakao } from '../view_model'
-import { MOauthKakao } from '../model';
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import Head from 'next/head'
-import { useEffect } fr
-import { Kakao } from '../apis/oauth/kakao';
-import styles from '../styles/Home.module.css'
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { MOauthKakao } from "../model/"
+import { useEffect } from 'react';
 
 
 export default function Home() {
@@ -14,7 +10,14 @@ export default function Home() {
   //   console.log(code);
   //   getKakaoTocken.mutate(code);
   // }, [])
+  // if (getKakaoTocken.isLoading) return <p>Loading...</p>
 
+  // if (getKakaoTocken.error) return <p>An error has occurred</p>
+
+  // if (getKakaoTocken.isSuccess) {
+  //   console.log(getKakaoTocken.data)
+  //   return <p> Success </p>
+  // }
   useEffect(() => {
     const queryString = new URL(window.location.href);
     const code: any = queryString.searchParams.get('code');
@@ -24,15 +27,7 @@ export default function Home() {
 
   }, [])
 
-  return;
-  // if (getKakaoTocken.isLoading) return <p>Loading...</p>
 
-  // if (getKakaoTocken.error) return <p>An error has occurred</p>
-
-  // if (getKakaoTocken.isSuccess) {
-  //   console.log(getKakaoTocken.data)
-  //   return <p> Success </p>
-  // }
   return (
     <>
     </>
