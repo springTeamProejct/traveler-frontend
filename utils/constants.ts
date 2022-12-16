b6d2e05d85959b626145dc18b51e7a3f45be4633
JopopScript <lssmm1230@email.com>
2022-12-17 04:00:39 +0900

    feat: react-query generic적용한 예시 폴더 생성

---
+++ utils/constants.ts
@@ -0,0 +1,11 @@
+const CONSTANTS = {
+    SERVER_URL: 'http://localhost:4000',
+    HTTP_METHOD: {
+        GET: 'get',
+        POST: 'post',
+        PATCH: 'PATCH',
+        DELETE: 'delete',
+    }
+}
+
+export { CONSTANTS }
