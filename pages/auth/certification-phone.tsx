29985ef341ce53985965233f3a5d97a782f39ac2
hwanju lee <hwanju1596@gmail.com>
2023-02-06 23:48:53 +0900

    #register 1. Controller함수랑 UI 분리.   - PhoneNumberInput UI 분리 2. 변수명 수정.(가독성 향상)

--- pages/auth/certification-phone.tsx
+++ pages/auth/certification-phone.tsx
@@ -14,12 +14,12 @@ import React, { useCallback, useEffect, useState } from "react";
 import { MuiOtpInput } from 'mui-one-time-password-input';
 import { useCountdownTimer } from "../../hooks/useCountdownTimer"
 import { useAuthMutation } from "../../apis/auth/signup";
-interface AuthButtonProps {
-  viewAuthInput: boolean;
-  setViewAuthInput: React.Dispatch<React.SetStateAction<boolean>>;
+interface AuthCodeSendBtnProps {
+  showAuthCodeInput: boolean;
+  setShowAuthCodeInput: React.Dispatch<React.SetStateAction<boolean>>;
   setBtnClicked: React.Dispatch<React.SetStateAction<boolean>>;
 }
-interface AuthInputProps {
+interface AuthCodeInputProps {
   setIsUser: React.Dispatch<React.SetStateAction<string>>;
   sendAuthBtn: boolean;
   authCode: string;
@@ -27,41 +27,58 @@ interface AuthInputProps {
 interface CertificaationPhoneProps {
   setIsUser: React.Dispatch<React.SetStateAction<string>>;
 }
-
+interface PhoneNumberInputPops {
+  phoneNumber: string;
+  setPhoneNumber: React.Dispatch<React.SetStateAction<string>>;
+}
+// This Page Main Controller
 export const CertificaationPhone = ({ setIsUser }: CertificaationPhoneProps) => {
   const [phoneNumber, setPhoneNumber] = useState<string>("");
-  const [viewAuthInput, setViewAuthInput] = useState<boolean>(false);
+  const [ShowAuthCodeInput, setShowAuthCodeInput] = useState<boolean>(false);
   const [btnClicked, setBtnClicked] = useState<boolean>(false);
   const [authCode, setAuthCode] = useState<string>('123456');
+  // 일단 한국만 폰인증.
+  let koreanPhoneNumber = (phoneNumber.slice(3)).replaceAll(' ', '');
+  const mutate = useAuthMutation('users/signup/authcode', 'phoneNum', koreanPhoneNumber);
 
-  const handlePhoneNumberChange = (newPhone: string, info: MuiTelInputInfo) => {
-    setPhoneNumber(newPhone);
-  };
-  const continents: MuiTelInputContinent[] = ["AS"];
-  const excludedCountries: MuiTelInputCountry[] = [];
+  // Send Auth Code
+  useEffect(() => {
+    // mutate.mutate();
+  }, [btnClicked])
 
   return (
     <Container maxWidth="xs">
       <Stack spacing={2}>
-        <MuiTelInput
-          fullWidth
-          defaultCountry="KR"
-          value={phoneNumber}
-          onChange={handlePhoneNumberChange}
-          continents={continents}
-          excludedCountries={excludedCountries}
-        />
-        <AuthButton viewAuthInput={viewAuthInput} setBtnClicked={setBtnClicked} setViewAuthInput={setViewAuthInput} />
+        <PhoneNumberInput phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber} />
+        <AuthCodeSendBtn showAuthCodeInput={ShowAuthCodeInput} setBtnClicked={setBtnClicked} setShowAuthCodeInput={setShowAuthCodeInput} />
         {
-          viewAuthInput &&
-          <AuthInput sendAuthBtn={btnClicked} setIsUser={setIsUser} authCode={authCode} />
+          ShowAuthCodeInput &&
+          <AuthCodeInput sendAuthBtn={btnClicked} setIsUser={setIsUser} authCode={authCode} />
         }
       </Stack>
     </Container>
   );
 };
 
-const AuthInput = ({ sendAuthBtn, setIsUser, authCode }: AuthInputProps) => {
+const PhoneNumberInput = ({ phoneNumber, setPhoneNumber }: PhoneNumberInputPops) => {
+  const continents: MuiTelInputContinent[] = ["AS"];
+  const excludedCountries: MuiTelInputCountry[] = [];
+
+  const handlePhoneNumberChange = (newPhone: string, info: MuiTelInputInfo) => {
+    setPhoneNumber(newPhone);
+  };
+
+  return (
+    <MuiTelInput
+      fullWidth
+      defaultCountry="KR"
+      value={phoneNumber}
+      onChange={handlePhoneNumberChange}
+      continents={continents}
+      excludedCountries={excludedCountries} />);
+};
+
+const AuthCodeInput = ({ sendAuthBtn, setIsUser, authCode }: AuthCodeInputProps) => {
   const [value, setValue] = useState<string>('');
   const { timeLeft, formattedTimeLeft, setTimeLeft } = useCountdownTimer(0);
   useEffect(() => {
@@ -104,16 +121,16 @@ const AuthInput = ({ sendAuthBtn, setIsUser, authCode }: AuthInputProps) => {
   );
 };
 
-const AuthButton = ({ viewAuthInput, setBtnClicked, setViewAuthInput }: AuthButtonProps) => {
+const AuthCodeSendBtn = ({ showAuthCodeInput, setBtnClicked, setShowAuthCodeInput }: AuthCodeSendBtnProps) => {
   const { timeLeft: reSendTimeLeft, formattedTimeLeft: formattedReSendTimeLeft, setTimeLeft: setReSendTimeLeft } = useCountdownTimer(0);
-  const mutate = useAuthMutation('users/signup/authcode', 'phoneNum', '01098598222');
 
-  const handleAuthButtonClick_ = useCallback(() => {
+  // 수정필요.
+  const handleAuthButtonClick = useCallback(() => {
     if (reSendTimeLeft === 0) {
       setBtnClicked((value) => !value);
-      setViewAuthInput(true);
+      setShowAuthCodeInput(true);
       setReSendTimeLeft(30);
-      mutate.mutate();
+      // mutate.mutate();
     }
   }, [formattedReSendTimeLeft, reSendTimeLeft]);
 
@@ -124,10 +141,10 @@ const AuthButton = ({ viewAuthInput, setBtnClicked, setViewAuthInput }: AuthButt
       type="submit"
       variant="contained"
       disabled={reSendTimeLeft > 0 ? true : false}
-      onClick={handleAuthButtonClick_}
+      onClick={handleAuthButtonClick}
     >
       {
-        viewAuthInput // 0: 인증번호전송, > 1: 재전송
+        showAuthCodeInput // 0: 인증번호전송, > 1: 재전송
           ? <>재전송 {formattedReSendTimeLeft} </>
           : <>인증번호전송</>
       }
